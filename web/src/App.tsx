778145dcd94e2341cfd64c0454b939a60b0b2f0c
Lisitha Kularathna <128106689+lisithakularathna@users.noreply.github.com>
2026-09-19 02:17:34 +0530

    Add following route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,6 +14,7 @@ import UserProfile from "./pages/UserProfile";
 import Search from "./pages/Search";
 import Activity from "./pages/Activity";
 import Messages from "./pages/Messages";
+import Following from "./pages/Following";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -75,6 +76,11 @@ function App() {
               <Search />
             </ProtectedRoute>
           }
+        />
+
+        <Route
+          path="/following"
+          element={<ProtectedRoute><Following /></ProtectedRoute>}
         />
 
         <Route
